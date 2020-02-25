3385b3f5b31249ef40fd4e450ef9874a9c098d96
Guilherme Caixeta <guilherme.lpcaixeta@gmail.com>
2020-02-25 15:41:12 +0000

    Initial commit

---
+++ CreateAppointment/DTO/AppointmentDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateAppointment.DTO
+{
+    public class AppointmentDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string ServiceName { get; set; }
+        public string Address { get; set; }
+    }
+}
